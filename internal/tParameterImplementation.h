61ca5284096fbfba3dfde5b6c125b0597b696401
Max Reichardt <mreichardt@gmx.net>
2013-04-29 02:00:13 +0200

    Adapted to changes (port listeners) in data_types plugin

--- internal/tParameterImplementation.h
+++ internal/tParameterImplementation.h
@@ -108,7 +108,7 @@ public:
     current_value = value;
   }
 
-  void PortChanged(data_ports::tInputPort<T>& origin, const T& value, const rrlib::time::tTimestamp& timestamp)
+  void PortChanged(const T& value, data_ports::tChangeContext& change_context)
   {
     Set(value);
   }
@@ -152,7 +152,7 @@ public:
     current_value = stored_value;
   }
 
-  void PortChanged(data_ports::tInputPort<T>& origin, const T& value, const rrlib::time::tTimestamp& timestamp)
+  void PortChanged(const T& value, data_ports::tChangeContext& change_context)
   {
     Set(value);
   }
